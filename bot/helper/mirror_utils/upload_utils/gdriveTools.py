701abccf7477f0265664184c20d908f32d11ea7d
xfuryy <81030085+xfuryy@users.noreply.github.com>
2022-01-02 23:34:02 +0545

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -550,7 +550,7 @@ class GoogleDriveHelper:
                     nxt_page += 1
             telegraph.edit_page(
                 path = self.path[prev_page],
-                title = 'Mirror-Leech-Bot Drive Search',
+                title = 'LeechX',
                 content=content
             )
         return
@@ -737,7 +737,7 @@ class GoogleDriveHelper:
         for content in self.telegraph_content:
             self.path.append(
                 telegraph.create_page(
-                    title='Mirror-Leech-Bot Drive Search',
+                    title='LeechX',
                     content=content
                 )["path"]
             )
